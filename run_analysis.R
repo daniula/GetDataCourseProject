9cd1d49ee20331fcab4efa49aa38059bb013d055
Daniel Mendalka <daniel.mendalka@gmail.com>
2015-02-20 06:44:51 +0100

    use require(dplyr) and check if it's installed

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,8 @@
-library(dplyr)
+if (!require("dplyr")) {
+    install.packages("dplyr")
+}
+
+require(dplyr)
 
 run_analysis <- function () {
     dirPrefix <- "."
@@ -7,39 +11,39 @@ run_analysis <- function () {
     if (file.exists("UCI HAR Dataset")) {
         dirPrefix <- "UCI HAR Dataset"
     }
-        
+
     # Load features to data.frame and extract vector with features' names.
     features <- read.table(paste(dirPrefix, "features.txt", sep="/"))[,2]
-    
+
     # Load both X_train and X_test files with features as column names and bind them together.
     X <- rbind(
         read.table(paste(dirPrefix, "train", "X_train.txt", sep="/"), col.names=features),
         read.table(paste(dirPrefix, "test", "X_test.txt", sep="/"), col.names=features)
     )
-    
+
     # Extract only columns which contains ".mean." or ".std." instead them.
     data <- X[,grep('\\.(mean|std)\\.', names(X))]
-        
+
     # Load both y_train and y_test and bind them as activity column
     data$activity <- rbind(
         read.table(paste(dirPrefix, "train", "y_train.txt", sep="/")),
-        read.table(paste(dirPrefix, "test", "y_test.txt", sep="/"))    
+        read.table(paste(dirPrefix, "test", "y_test.txt", sep="/"))
     )[,1]
-    
+
     # Load activity_labels and use them in activity column to replace
     # nondescriptive numbers to descriptive labels
     activities <- read.table(paste(dirPrefix, "activity_labels.txt", sep="/"))[,2]
     data$activity <- activities[data$activity]
-        
+
     # Load and bind subject_train and subject_test to subject columns
     data$subject <- rbind(
         read.table(paste(dirPrefix, "train", "subject_train.txt", sep="/")),
-        read.table(paste(dirPrefix, "test", "subject_test.txt", sep="/"))        
+        read.table(paste(dirPrefix, "test", "subject_test.txt", sep="/"))
     )[,1]
-    
+
     # Output created dataset into data.txt file
     write.table(data, file = "data.txt", row.name = FALSE)
-    
+
     # Compute average of each variable for each activity and each subject
     # and save it to summary.txt
     summary <- data %>% group_by(activity, subject) %>% summarise_each(funs(mean))
